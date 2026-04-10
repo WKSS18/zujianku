66e00425df00f8e9e34d2d399e13c17b25965094
王浩 <hao.wang@1825254875506504.onaliyun.com>
2026-04-10 11:42:23 +0800

    fix: 测试

--- demo/src/App.tsx
+++ demo/src/App.tsx
@@ -1,10 +1,12 @@
 // @ts-nocheck
-import { SearchForm } from '@test111190909222/components'
+// import { SearchForm } from '@test111190909222/components'
 import './App.css'
+import { SearchForm,Button } from '@sh-ai/work-ui'
 
 function App() {
   return (
     <>
+      <Button type='primary'>123</Button>
       <SearchForm fields={[
         {
           label: 'Name',
